2717cfcc0e7c880a45d65341cd4c32014834a64f
hassan45-5 <148480926+Hassan45B@users.noreply.github.com>
2025-03-19 12:28:21 +0300

    add styling for Contact section and integrate it into main application

--- Portfolio/src/main.jsx
+++ Portfolio/src/main.jsx
@@ -7,6 +7,7 @@ import Content from './content.jsx'
 import Experience from './Experience.jsx'
 import Projects from './Projects.jsx'
 import Articles from './Articles.jsx'
+import Contact from './Contact.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -16,5 +17,6 @@ createRoot(document.getElementById('root')).render(
     <Content/>
     <Experience/>
     <Projects/>
+    <Contact/>
   </StrictMode>,
 )

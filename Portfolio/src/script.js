5285a0aa726f66e5786b1e7fde658e0e0dccb130
hassan45-5 <148480926+Hassan45B@users.noreply.github.com>
2025-03-19 08:05:00 +0300

    add theme toggle functionality with light/dark mode support in index.html and script.js

--- Portfolio/src/script.js
+++ Portfolio/src/script.js
@@ -6,3 +6,19 @@ var TYPED = new Typed("#typed", {
     showCursor: false,
     loop: true
 });
+
+
+function them(){
+    const darkBtn = document.getElementById('darkBtn');
+    const lightBtn = document.getElementById('lightBtn');
+
+    document.body.classList.toggle('light');
+
+    if (document.body.classList.contains('light')) {
+        darkBtn.style.display = 'block';
+        lightBtn.style.display = 'none';
+    }else{
+        darkBtn.style.display = 'none';
+        lightBtn.style.display = 'block';
+    }
+}

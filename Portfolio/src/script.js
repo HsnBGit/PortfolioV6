ae9469b44c8e984a6f7da3248d6ace1080e56981
hassan45-5 <148480926+Hassan45B@users.noreply.github.com>
2025-03-19 07:59:08 +0300

    add dynamic typing effect to header and include new script for enhanced user experience

---
+++ Portfolio/src/script.js
@@ -0,0 +1,8 @@
+var TYPED = new Typed("#typed", {
+    strings: ['Software Engineer', "AI Engineer", "Front-end Developer"],
+    typedSpeed: 50,
+    backSpeed: 20,
+    backDelay: 3000,
+    showCursor: false,
+    loop: true
+});

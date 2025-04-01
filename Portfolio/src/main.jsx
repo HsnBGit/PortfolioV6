093b621cb2b7ec13af24f4887feee735127a65c1
hassan45-5 <148480926+Hassan45B@users.noreply.github.com>
2025-04-01 17:49:10 +0300

    fix duplicate import statements in main.jsx

--- Portfolio/src/main.jsx
+++ Portfolio/src/main.jsx
@@ -7,6 +7,8 @@ import Content from './content.jsx'
 import Experience from './Experience.jsx'
 import Projects from './Projects.jsx'
 import Articles from './Articles.jsx'
+import Articles from './Articles.jsx'
+import Articles from './Articles.jsx'
 import Contact from './Contact.jsx'
 
 createRoot(document.getElementById('root')).render(
